614e8558dfd45afa67dc70795cacd72ce48fd799
mxdi9i7 <peter.zheng88228@gmail.com>
2021-06-12 17:01:40 -0400

    updates

--- src/routes/post/controller.ts
+++ src/routes/post/controller.ts
@@ -15,7 +15,18 @@ const { OK } = StatusCodes;
  */
 export async function getAllPosts(req: Request, res: Response) {
   const posts = await PostModel.find({});
-  return res.status(OK).json(posts);
+  const result = [];
+  for (let i = 0; i < posts.length; i++) {
+    const authorId = posts[i].author;
+    const author = await UserModel.findById(authorId);
+    const postObject = posts[i].toJSON();
+    result.push({
+      ...postObject,
+      author,
+      createdAt: posts[i]._id.getTimestamp(),
+    });
+  }
+  return res.status(OK).json(result);
 }
 
 /**
@@ -35,4 +46,17 @@ export async function getPostById(req: Request, res: Response) {
     authorName: author?.username,
     authorAvatar: author?.avatar,
   });
+}
+
+/**
+ * Create post with author and title.
+ *
+ * @param req
+ * @param res
+ * @returns
+ */
+export async function createPost(req: Request, res: Response) {
+  const { author, title } = req.body;
+  const post = await PostModel.create({ author, title });
+  return res.status(OK).json(post);
 }
--- src/routes/post/index.ts
+++ src/routes/post/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getAllPosts, getPostById } from './controller';
+import { createPost, getAllPosts, getPostById } from './controller';
 
 const postRouter = Router();
 
 postRouter.get('/', getAllPosts);
 postRouter.get('/:id', getPostById);
+postRouter.post('/', createPost);
 
 export default postRouter;
